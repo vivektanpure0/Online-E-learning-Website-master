2097dd1261ddbb0ae32ad4b0eef344dce96a802e
vivektanpure0 <vivektanpure0@gmail.com>
2021-08-10 01:57:44 +0530

    C
    C

--- src/Controller/Course.js
+++ src/Controller/Course.js
@@ -7,6 +7,8 @@ import CourseJava from './CourseJava';
 
 let Course = () => {
     const [courseList, setCourseList] = useState([]); // from axios
+    // const [parentcourseList, setParentCourseList] = useState({}); // parent state  
+    // const [parentcourseListHike, setParentCourseListHke] = useState(0); // parent state  
     
     return (
         <div>
@@ -22,6 +24,54 @@ let Course = () => {
 }
 export default Course;
 
+
+// import React from 'react';
+// import { useState, useEffect } from 'react';
+// import Javadata from './Javadata';
+// // import Counter from './Counter';
+
+// //*FUNCTION COMPONANT*
+
+// let Moderator = () => {
+//     const [emp, setEmp] = useState('');
+//     const [parentEmp, setParentEmp] = useState({}); // parent state  
+//     const [parentEmpHike, setParentEmpHke] = useState(0); // parent state  
+//     // const [childEmp, setChildEmp] = useState({}); // from callback    
+
+//     // useEffect(() => {
+
+//     //     setParentEmp({
+//     //         mid: 201,
+//     //         mname: 'Monu',
+//     //         memail: 'monu@gmail.com'
+//     //     }
+//     //     );
+
+//     //     setParentEmpHke(10);
+//     // }, []);
+
+//     // const handleCallback = (childEmpData) => {
+//     //     setChildEmp(childEmpData);
+//     //     console.log(childEmpData);
+//     // }
+
+//     return (
+//         <div>
+//             <h1 >Moderator Component</h1>
+//             {/* <p> parent {parentEmp.mname}</p> */}
+//             {/* <p> parent {parentEmpHike}</p> */}
+//             {/* <JavaData></JavaData> */}
+//             {/* <p>parent {childEmp.name}</p> */}
+//             <Javadata
+//                 // parentEmp={parentEmp}
+//                 // parentEmpHike={parentEmpHike}
+//                 // parentCallback={handleCallback}
+//             ></Javadata>
+//             {/* <Counter></Counter> */}
+//         </div>
+//     )
+// }
+// export default Moderator;
 
 
 
--- src/Controller/CourseJava.js
+++ src/Controller/CourseJava.js
@@ -35,3 +35,73 @@ export default CourseJava;
 
 
 
+// import axios from "axios";
+// import { useState } from 'react';
+
+// let JavaData = (props) => {
+//     const [empList, setEmpList] = useState([]); // from axios
+//     // const [parentEmp, setParentEmp] = useState({}); // from props  
+//     // const [parentEmpHike, setParentEmpHke] = useState(0); // from props 
+//     //const [childEmp, setChildEmp] = useState({}); // child state   
+//     // const [emp, setEmp] = useState({}); // from axios 
+
+//     // initilize states to prevent undefined error, in case used these fields anywhere else in future 
+//     // useEffect(() => {
+//     //     setParentEmp(props.parentEmp); // 4
+//     //     setParentEmpHke(props.parentEmpHike);
+//     //     setChildEmp({
+//     //         mid: 301,
+//     //         mname: 'Tonu',
+//     //         memail: 'tonu@gmail.com'
+//     //     }
+//     //     );
+//     //     setEmp({
+//     //         mid: 0,
+//     //         mname: '',
+//     //         memail: ''
+//     //     }
+//     //     );
+
+//     // }, []);
+
+//     // const onTrigger = () => {
+//     //     props.parentCallback(childEmp);
+//     // };
+
+//     // const handleEmployee = (event) => {
+//     //     console.log(emp.mid);
+//     //     setEmp({
+//     //         ...emp,
+//     //         [event.target.name]: event.target.value
+//     //     });
+//     // };
+
+//     const getallmoderator = (e) => {
+//         console.log();
+//         axios.get(`/getallmoderator`)
+//             .then((response) => {
+//                 setEmpList(response.data);
+//             }).catch(error => {
+//                 console.log(error.message)
+//             });
+//     }
+
+//     return (
+//         <div>
+//             <p>JavaData Component</p>
+//             {/* <p>axios {emp.mname}</p>
+//             <p>child {parentEmp.mname}</p>
+//             <p> child {parentEmpHike} </p> */}
+//             {/* <button onClick={onTrigger}>Pass child emp to parent</button> */}
+//             <div>
+//                     <p>Get all Moderator data </p>
+//                     <button type="submit" className="btn btn-primary" onClick={getallmoderator}>Get All Moderator</button>
+//                     <p>All Moderator Data</p>
+//                     <div> {empList.map(e => <p> {e.mid}, {e.mname} {e.memail} </p>)} </div>
+//             </div>
+
+//         </div>
+       
+//     )
+// }
+// export default JavaData;
